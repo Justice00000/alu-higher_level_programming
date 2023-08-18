4f350114d1bbfe67af5d0b93a72ac827d29c08eb
Justice Chukwuonye <j.chukwuony@alustudent.com>
2023-08-18 03:17:21 -0700

    Loading

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,21 +1,22 @@
-#!/usr/bin/python3
-'''a script that lists all State objects from the database hbtn_0e_6_usa
-'''
-from sqlalchemy import create_engine
-import sys
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+-- Create states table in hbtn_0e_101_usa with some data
+CREATE DATABASE IF NOT EXISTS hbtn_0e_101_usa;
+USE hbtn_0e_101_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
+INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
 
-if __name__ == "__main__":
-    engine = create_engine(
-        "mysql+mysqldb://{user}:{passwd}@localhost:3306/{db}".format(
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
-            pool_pre_ping=True)
-    )
-    engine.connect()
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    for state in session.query(State).order_by(State.id):
-        print(f"{state.id}: {state.name}"))
+CREATE TABLE IF NOT EXISTS cities (
+    id INT NOT NULL AUTO_INCREMENT,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY(state_id) REFERENCES states(id)
+);
+INSERT INTO cities (state_id, name) VALUES (1, "San Francisco"), (1, "San Jose"), (1, "Los Angeles"), (1, "Fremont"), (1, "Livermore");
+INSERT INTO cities (state_id, name) VALUES (2, "Page"), (2, "Phoenix");
+INSERT INTO cities (state_id, name) VALUES (3, "Dallas"), (3, "Houston"), (3, "Austin");
+INSERT INTO cities (state_id, name) VALUES (4, "New York");
+INSERT INTO cities (state_id, name) VALUES (5, "Las Vegas"), (5, "Reno"), (5, "Henderson"), (5, "Carson City");
