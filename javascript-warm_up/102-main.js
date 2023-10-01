0963ea4fd8558867e8e62ff75b8a4126ed9ad061
Justice Chukwuonye <j.chukwuony@alustudent.com>
2023-10-01 00:50:07 -0700

    ALU JS

---
+++ javascript-warm_up/102-main.js
@@ -0,0 +1,7 @@
+
+Executable File·5 lines (5 loc) · 146 Bytes
+#!/usr/bin/node
+const addMeMaybe = require('./102-add_me_maybe').addMeMaybe;
+addMeMaybe(4, function (nb) {
+  console.log('New value: ' + nb);
+});
